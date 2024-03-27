e4fd2ff4bc8ca060113906bc35dcf37c08a6bdf3
Daniil Videneev <danyavid@gmail.com>
2024-03-27 20:16:50 +0300

    Update stemming for words with apostrophe & hyphen

--- stemming.go
+++ stemming.go
@@ -7,6 +7,13 @@ import (
 	"unicode"
 )
 
+var stemChain = []func(string) (string, bool, error){
+	stemIgnore,
+	stemApostrophe,
+	stemHyphen,
+	stemInternal,
+}
+
 func stem(s string) ([]string, error) {
 	words := strings.FieldsFunc(s, func(r rune) bool {
 		return !(unicode.IsLetter(r) || r == '\'' || r == '-')
@@ -14,36 +21,73 @@ func stem(s string) ([]string, error) {
 
 	stemmedWordsSet := make(map[string]struct{})
 	for _, v := range words {
-		if shouldIgnore(v) {
+		var res string
+		for _, f := range stemChain {
+			if stemmed, ok, err := f(v); err != nil {
+				return []string{}, err
+			} else if ok {
+				res = stemmed
+				break
+			}
+		}
+
+		if len(res) == 0 {
 			continue
 		}
-
-		stemmed, err := snowball.Stem(v, "english", false)
-		if err != nil {
-			return nil, err
-		}
-
-		stemmedWordsSet[stemmed] = struct{}{}
+		stemmedWordsSet[res] = struct{}{}
 	}
 
-	var stemmedWordsSlice []string
-	for v, _ := range stemmedWordsSet {
+	stemmedWordsSlice := make([]string, 0, len(stemmedWordsSet))
+	for v := range stemmedWordsSet {
 		stemmedWordsSlice = append(stemmedWordsSlice, v)
 	}
 
 	return stemmedWordsSlice, nil
 }
 
-func shouldIgnore(s string) bool {
-	if isStopWord(s) {
-		return true
+func stemIgnore(word string) (string, bool, error) {
+	if isStopWord(word) {
+		return "", true, nil
 	}
 
-	ind := strings.IndexFunc(s, func(r rune) bool {
-		return r == '\''
-	})
+	return word, false, nil
+}
 
-	return ind > 0
+func stemApostrophe(word string) (string, bool, error) {
+	ind := strings.Index(word, "'")
+	if ind < 0 {
+		return word, false, nil
+	}
+
+	_, ignore, err := stemIgnore(word[:ind])
+	if ignore || err != nil {
+		return "", true, err
+	}
+
+	return word, true, nil
+}
+
+func stemHyphen(word string) (string, bool, error) {
+	ind := strings.LastIndex(word, "-")
+	if ind < 0 {
+		return word, false, nil
+	}
+
+	stemmed, _, err := stemInternal(word[ind+1:])
+	if err != nil {
+		return "", true, err
+	}
+
+	return word[:ind+1] + stemmed, true, nil
+}
+
+func stemInternal(word string) (string, bool, error) {
+	stemmed, err := snowball.Stem(word, "english", false)
+	if err != nil {
+		return "", true, err
+	}
+
+	return stemmed, true, nil
 }
 
 func isStopWord(s string) bool {
